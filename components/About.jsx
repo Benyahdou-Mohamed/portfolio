7d234930a674a6e4e68f7e37960a4b4233e7fb37
benyahdou-mohamed <benyahdou.moh29@hotmail.com>
2024-03-30 17:03:49 +0100

    ss

--- components/About.jsx
+++ components/About.jsx
@@ -4,6 +4,7 @@ const About = () => {
   return (
     <section>
     <div className=' about_section font-mono font-bold text-center hero_section w-full flex pt-32 px-32  justify-center'>Empowering all-scale startups with custom web experiences that are memorable and drive success. With a passion for design and development, I take projects from ideation to launch, ensuring a seamless journey that leaves a lasting impact in the digital landscape. </div>
+    gere is 
     </section>
   )
 }
